56998d131ae3be523cffe4c6f5e8ba130c504d64
John Ford <john@johnford.info>
2014-06-11 15:25:45 -0700

    Script to manually commit to gaia-try

---
+++ manual.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+if [ x"$1" == x ] ; then echo "Missing remote" ; exit 1 ; fi
+if [ x"$2" == x ] ; then echo "Missing commitish" ; exit 1 ; fi
+
+echo "Pushing to try"
+echo "Remote: $1"
+echo "Commitish: $2"
+
+rm -rf .gaia-try
+hg clone http://hg.mozilla.org/integration/gaia-try .gaia-try
+python -c "
+import json
+import random
+with open('.gaia-try/gaia.json', 'w+') as f:
+  data={
+    'git': {
+        'git_revision': '$2',
+        'remote': '$1',
+    },
+    random.randint(0,10000000): 'random'
+  }
+  json.dump(data, f, indent=2)
+"
+hg -R .gaia-try commit -m "Manually pushing $1 @ $2 to try $MSG"
+if [ -z $NOPUSH ] ; then
+    hg -R .gaia-try push -f ssh://hg.mozilla.org/integration/gaia-try
+fi
