89779b4f0e6f63997737d604799450db3e9e12ad
John Ford <john@johnford.info>
2014-06-16 18:13:57 -0700

    Make Connection an event emitter

--- msg_broker.js
+++ msg_broker.js
@@ -3,40 +3,76 @@
 var url = require('url');
 var when = require('when');
 var amqp = require('amqplib');
+var debug = require('debug')('try-server-hook:msg_broker');
+var util = require('util');
+var events = require('events');
 
 function Connection (uri, opts) {
   this.uri = uri;
-  this.options = {};
-  this.channels = [];
+  this.channelCount = 0;
   this.options = opts || {};
   if (!this.options.heartbeat) {
     this.options.heartbeat = 15 * 60;
   }
+  events.EventEmitter.call(this);
 }
 
-Connection.prototype = {
-  open: function () {
-    return when(amqp.connect(this.uri, this.heartbeat).then(function(conn) {
-      // We only print the hostname to avoid showing credentials in output
-      console.log('Connected message broker running at ' + url.parse(this.uri).hostname);
-      this.connection = conn;
-      return when.resolve(conn);
-    }.bind(this)));
-  },
-  // Probably should do something to remove the channels when they are finished with
-  createChannel: function () {
-    if (!this.connection) {
-      return when.reject('Cannot create channel before connection is established');
-    }
-    return when(this.connection.createChannel().then(function(ch) {
-      console.log('Opened a channel'); 
-      this.channels.push(ch);
-      return ch;
-    }.bind(this)));
-  },
-  close: function() {
-    return this.connection.close();
+util.inherits(Connection, events.EventEmitter);
+
+Connection.prototype.open = function () {
+  return when(amqp.connect(this.uri, this.options).then(function(conn) {
+    // We only print the hostname to avoid showing credentials in output
+    debug('Connected message broker running at ' + url.parse(this.uri).hostname);
+    debug('Heartbeats every %s seconds', this.options.heartbeat);
+    this.emit('connected');
+    this.connection = conn;
+    conn.on('close', function() {
+      console.log('Closing AMQP connection')
+    });
+    conn.on('error', function(e) {
+      console.log(e);
+      console.log('ERROR: AMQP Connection Error\n%s', e.stack||e)
+      this.emit('error', e);
+    }.bind(this));
+    conn.on('blocked', function(reason) {
+      debug('AMQP connection blocked: ', reason);
+      this.emit('blocked', reason);
+    }.bind(this));
+    conn.on('unblocked', function() {
+      debug('AMQP connection unblocked');
+      this.emit('unblocked');
+    }.bind(this));
+    return when.resolve(conn);
+  }.bind(this)));
+};
+
+Connection.prototype.createChannel = function () {
+  if (!this.connection) {
+    return when.reject('Cannot create channel before connection is established');
   }
+  return when(this.connection.createChannel().then(function(ch) {
+    debug('Opened a channel');
+    this.channelCount++;
+    ch.on('close', function() {
+      this.channelCount--;
+      debug('Closing AMQP channel, %d channels remaining', this.channelCount);
+      this.emit('chClose', channelCount);
+      if (channelCount === 0) {
+        this.emit('lastChClose');
+      }
+    }.bind(this));
+    ch.on('error', function(e) {
+      console.log(e);
+      console.log('ERROR: AMQP Channel Error\n%s', e.stack||e);
+      this.emit('chErr', e);
+    });
+    return ch;
+  }.bind(this)));
+};
+
+Connection.prototype.close = function() {
+  this.emit('close');
+  return this.connection.close();
 };
 
 module.exports = Connection;
